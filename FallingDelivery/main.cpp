549f9132b58165b2b50e59e9283b43f9dff7c943
Quinten365 <staut.quinten@gmail.com>
2026-05-22 11:31:28 +0200

    Objects samenVoegen aan main

--- FallingDelivery/main.cpp
+++ FallingDelivery/main.cpp
@@ -5,16 +5,37 @@
 #include <vector>
 
 int main() {
-    const int screenWidth = 1200;
+    const int screenWith = 1200;
     const int screenHeight = 800;
-    InitWindow(screenWidth, screenHeight, "Falling Delivery");
+    std::vector<game::Objects*> objects;
+    auto object = new game::Objects("FallingDelivery/assets/hellportal.png");
+    InitWindow(screenWith, screenHeight, "Falling Delivery");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_S)) {
+            objects.push_back(new game::Objects("FallingDelivery/assets/hellportal.png"));
+        }
         BeginDrawing();
         ClearBackground(SKYBLUE);
+        for (game::Objects* o: objects) {
+            o->Update();
+            o->Draw();
+        }
+        auto it = objects.begin();
+        while (it != objects.end()) {
+            game::Objects* o = *it;
 
+            if (o->GetPosition().y  < 0) {
+                delete o;
+                it =objects.erase(it);
+                std::cout << "object removed";
+            }
+            else {
+                ++it;
+            }
+        }
         EndDrawing();
     }
 
