e3b1ad519f524ed48f7126eafef99a65301f55fb
Quinten365 <staut.quinten@gmail.com>
2026-05-22 10:57:01 +0200

    mainAanpassen

--- FallingDelivery/main.cpp
+++ FallingDelivery/main.cpp
@@ -2,18 +2,17 @@
 #include "raylib.h"
 #include "Objects.h"
 #include "Player.h"
+#include <vector>
 
 int main() {
-    const int screenWith = 1200;
+    const int screenWidth = 1200;
     const int screenHeight = 800;
-
-    InitWindow(screenWith, screenHeight, "Falling Delivery");
+    InitWindow(screenWidth, screenHeight, "Falling Delivery");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(SKYBLUE);
 
         EndDrawing();
