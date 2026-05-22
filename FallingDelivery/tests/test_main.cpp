cd8ee552b1558255a30039ea1f82b4545ab0e7f6
Quinten365 <staut.quinten@gmail.com>
2026-05-22 15:53:26 +0200

    unitTests

--- FallingDelivery/tests/test_main.cpp
+++ FallingDelivery/tests/test_main.cpp
@@ -1,21 +1,63 @@
 #include <catch2/catch_test_macros.hpp>
-#include "Held.h"
+#include "Objects.h"
+#include "Pakketje.h"
+#include "Player.h"
+//----------------------object-------------------------------
+TEST_CASE("sanity check object", "[Setup]") {
+    game::Objects o = game::Objects();
 
-TEST_CASE("sanity check", "[Setup]") {
-    Game::Held m = Game::Held();
-
-    REQUIRE_NOTHROW(m.GetPosition());
+    REQUIRE_NOTHROW(o.GetPosition());
 }
 
-TEST_CASE("Held", "[Held][spawnposition][x]") {
-    Game::Held _hero = Game::Held();
+TEST_CASE("Object", "[Object][spawnposition][x]") {
+    game::Objects object = game::Objects();
     SECTION("positie") {
         SECTION("X") {
             SECTION("Groterdan 0") {
-                REQUIRE(_hero.GetPosition().x>0);
+                REQUIRE(object.GetPosition().x>0);
             }
             SECTION("kleinerdan 800") {
-                REQUIRE(_hero.GetPosition().x<800);
+                REQUIRE(object.GetPosition().x<800);
+            }
+        }
+    }
+}
+//----------------------player-------------------------------
+TEST_CASE("sanity check player", "[Setup]") {
+    game::Player p = game::Player("assets/Skydiver.png");
+
+    REQUIRE_NOTHROW(p.GetPosition());
+}
+
+TEST_CASE("Player", "[Player][spawnposition][x]") {
+    game::Player player = game::Player("assets/Skydiver.png");
+    SECTION("positie") {
+        SECTION("X") {
+            SECTION("Groterdan 0") {
+                REQUIRE(player.GetPosition().x>0);
+            }
+            SECTION("kleinerdan 800") {
+                REQUIRE(player.GetPosition().x<800);
+            }
+        }
+    }
+}
+//----------------------pakketje-------------------------------
+TEST_CASE("sanity check pakketje", "[Setup]") {
+    game::Pakketje p = game::Pakketje("assets/pakketje.png");
+
+    REQUIRE_NOTHROW(p.GetPosition());
+}
+
+TEST_CASE("Pakketje", "[Pakketje][spawnposition][x]") {
+    game::Pakketje pakketje = game::Pakketje("assets/pakketje.png");
+    SECTION("positie") {
+        SECTION("X") {
+            SECTION("Groterdan 0") {
+                REQUIRE(pakketje.GetPosition().x>0);
+            }
+            SECTION("kleinerdan 800") {
+                REQUIRE(pakketje.GetPosition().x<800);
             }
         }
     }
