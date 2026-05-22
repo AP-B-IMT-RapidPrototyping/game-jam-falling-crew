312ab7107dd5cb262716b63a0815af4e608198d5
Quinten365 <staut.quinten@gmail.com>
2026-05-22 10:10:45 +0200

    Classes aanmaken

---
+++ FallingDelivery/src/Player.cpp
@@ -0,0 +1,8 @@
+//
+// Created by staut on 5/22/2026.
+//
+
+#include "Player.h"
+
+namespace Game {
+} // Game
---
+++ FallingDelivery/src/Player.h
@@ -0,0 +1,13 @@
+//
+// Created by staut on 5/22/2026.
+//
+
+#ifndef FALLINGDELIVERY_PLAYER_H
+#define FALLINGDELIVERY_PLAYER_H
+
+namespace Game {
+    class Player {
+    };
+} // Game
+
+#endif //FALLINGDELIVERY_PLAYER_H
