a6cc9331d22f7e1e900199a0ce29bec827efa8e4
Quinten365 <staut.quinten@gmail.com>
2026-05-22 10:11:04 +0200

    Objects aanmaken

---
+++ FallingDelivery/src/Objects.cpp
@@ -0,0 +1,8 @@
+//
+// Created by staut on 5/22/2026.
+//
+
+#include "Objects.h"
+
+namespace game {
+} // game
---
+++ FallingDelivery/src/Objects.h
@@ -0,0 +1,13 @@
+//
+// Created by staut on 5/22/2026.
+//
+
+#ifndef FALLINGDELIVERY_OBJECTS_H
+#define FALLINGDELIVERY_OBJECTS_H
+
+namespace game {
+    class Objects {
+    };
+} // game
+
+#endif //FALLINGDELIVERY_OBJECTS_H
