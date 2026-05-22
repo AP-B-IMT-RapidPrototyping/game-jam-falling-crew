47571af35fd9c440ccb87bf0136ad2767835c3f3
LeonV0801 <5n4gztzs8s@privaterelay.appleid.com>
2026-05-22 11:30:18 +0200

    player v1
    v1 player

--- FallingDelivery/src/Player.h
+++ FallingDelivery/src/Player.h
@@ -5,8 +5,21 @@
 #ifndef FALLINGDELIVERY_PLAYER_H
 #define FALLINGDELIVERY_PLAYER_H
 
+#include "raylib.h"
+#include <string>
+
 namespace game {
     class Player {
+    private:
+        Texture2D image;
+        Vector2 position;
+        float speed;
+    public:
+        Player(const std::string& imagePath);
+        void Draw();
+        void Update();
+        Vector2 GetPosition();
+        ~Player();
     };
 } // Game
 
